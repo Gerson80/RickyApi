19b7bf010aa4677bf429c2f3f112dc915d8ae944
Gerson Herrera <gerson.19070034@gmail.edu.mx>
2023-02-24 16:39:00 -0600

    Modificacion diseño inicio

---
+++ src/components/Navegacion.vue
@@ -0,0 +1,23 @@
+<template>
+
+      <div class=" overflow-hidden shadow-lg w-full" name="form1">
+        <div class="px-6  text-white text-xl bg-gradient-to-r from-yellow-300 via-lime-400 to-green-500">
+       
+            <nav class="text-center text-xl grid gap-4 grid-cols-2">
+                <div>
+                    <RouterLink to="/">Inicio</RouterLink> 
+                </div>
+                <div>
+                    <RouterLink to="/personajes">Personajes</RouterLink>
+                </div>
+       
+
+                
+            </nav>
+            
+            
+
+        </div>
+    </div>
+
+</template>
